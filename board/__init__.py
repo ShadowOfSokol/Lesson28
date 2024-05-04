7a821e1b859567f19dd1d215e91fb240aca242d8
ShadowOfSokol <sokolenkoegor19702002@gmail.com>
2024-05-04 15:55:18 +0300

    add gitignore

--- board/__init__.py
+++ board/__init__.py
@@ -12,10 +12,23 @@
 #__________________________________________________________________________________________________________________
 
 from flask import Flask
-from board import pages
-from board import posts
+from board import pages, posts, database
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_prefixed_env()
+
+    database.init_app(app)
+
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
+
+    print(f"Current ENVIRONMENT: {os.getenv('ENVIRONMENT')}")
+    print(f"Using Database: {app.config.get('DATABASE')}")
+
     return app
